3b6b04b6ee8918b3bbab41fbba1cc6c79afaab25
Abass016 <kolapoabass0@gmail.com>
2024-09-27 12:04:48 +0100

    I just added a new ntext into my python file

---
+++ Ali.py
@@ -0,0 +1,11 @@
+def MrAli():
+    """This is About me"""
+    print("My name is Ali")
+    print("I am Observing myy enterpreneurship here at Elegance tech hub")
+    print("I am a Gender Advocate")
+    print("I am A YAHOO BOY")
+    print("I as well added Yahoo plus to my game")
+    print("I play sporty bet for a living")
+    print("I used to wear Muslim Sohgit cap")
+
+MrAli()    
